2677ab0224fadf8b79a1ebf04a7dc5199e693ddc
Gagan Kumar <31625778+sgagankumar@users.noreply.github.com>
2019-06-10 17:06:22 +0530

    Update FastestTrain.c

--- FastestTrain.c
+++ FastestTrain.c
@@ -1,3 +1,24 @@
+/*Roy and Alfi reside in two different cities, Roy in city A and Alfi in city B. Roy wishes to meet her in city B.
+
+There are two trains available from city A to city B. Roy is on his way to station A (Railway station of city A). It will take T0 time (in minutes) for Roy to reach station A. The two trains departs in T1 and T2 minutes respectively. Average velocities (in km/hr) of trains are V1 and V2 respectively. We know the distance D (in km) between city A and city B. Roy wants to know the minimum time (in minutes) to reach city B, if he chooses train optimally.
+
+If its not possible to reach city B, print "-1" instead (without quotes).
+
+Note: If the minimum time is not integral, round the value to the least integer greater than minimum time.
+
+Input:
+
+First line will contain integer T, number of test cases.
+
+Second line will contain integers **T0,T1,T2,V1,V2,D  ** (their meanings are mentioned in problem statement)
+
+Output:
+
+Print the integral result, minimum time (in minutes) to reach city B in a new line.
+
+*/
+
+
 #include <stdio.h>
 #include <math.h>
 
